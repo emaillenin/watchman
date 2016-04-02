0a3ae7b8af2441e667114c1e8c4f36d975aa83a6
Lenin <emaillenin@gmail.com>
2016-04-02 13:56:16 +0530

    Use UTF8 cleaner while creating the email body

--- file_mailer.rb
+++ file_mailer.rb
@@ -1,11 +1,13 @@
 require 'action_mailer'
+require 'utf8-cleaner'
+
 class FileMailer < ActionMailer::Base
 
   def experror(files, to_email)
     puts "Sending email on #{files.length} changed files\n"
     body = ''
     files.each { |f| body << tail_file(f)}
-    mail(subject: '[Watchman] Files were modified', content_type: 'text/html', body: body.gsub("\n",'<br/>'), from: to_email, to: to_email)
+    mail(subject: '[Watchman] Files were modified', content_type: 'text/html', body: body.gsub("\n",'<br/>').encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: ''), from: to_email, to: to_email)
   end
 
   def tail_file(f)
--- watchman.rb
+++ watchman.rb
@@ -1,7 +1,6 @@
 require 'yaml'
 require 'action_mailer'
 require 'utf8-cleaner'
-# use UTF8Cleaner::Middleware
 
 require "#{File.expand_path(File.dirname(__FILE__))}/file_mailer"
 
