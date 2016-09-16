86d87b129c15ef615fe780b541ac5a140080c03a
Lenin <lenin.rajasekaran@bigcommerce.com>
2016-09-16 18:39:23 +1000

    Ignore empty files

--- file_mailer.rb
+++ file_mailer.rb
@@ -5,7 +5,11 @@ class FileMailer < ActionMailer::Base
   def experror(files, to_email)
     puts "Sending email on #{files.length} changed files\n"
     body = ''
-    files.each { |f| body << tail_file(f)}
+    files.each do |f|
+      line_count = `wc -l "#{f}"`.strip.split(' ')[0].to_i
+      next if line_count == 0 # Ignore empty files
+      body << tail_file(f)
+    end
     mail(subject: '[Watchman] Files were modified', content_type: 'text/html', body: body.gsub("\n",'<br/>').encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: ''), from: to_email, to: to_email)
   end
 
